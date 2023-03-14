83eac472dde5a726be814fd89d647fd173807f3b
Angela <uo284342@uniovi.es>
2023-03-14 12:30:27 +0100

    acabado 100% cobertura

--- src/main/java/colaDoble/DequeNode.java
+++ src/main/java/colaDoble/DequeNode.java
@@ -1,54 +1,106 @@
 package colaDoble;
+
 /**
- * Class representing a node of a double-ended queue (deque). Each node has references to
- * the next and previous nodes.
- * The previous and next of the first and last node of the deque is {@code null}.
+ * Esta clase representa un nodo de una lista doblemente enlazada. Cada nodo tiene una referencia a su
+ * elemento, así como a los nodos anteriores y siguientes.
  *
- * @param <T> the type of elements held in the deque.
+ * @param <T> el tipo de elemento almacenado en el nodo.
  */
 class DequeNode<T> {
     private T item;
     private DequeNode<T> previous;
     private DequeNode<T> next;
 
+    /**
+     * Crea un nuevo nodo con el elemento especificado, el nodo anterior y el nodo siguiente.
+     *
+     * @param item     el elemento almacenado en este nodo.
+     * @param previous el nodo anterior a este nodo.
+     * @param next     el nodo siguiente a este nodo.
+     */
     DequeNode(T item, DequeNode<T> previous, DequeNode<T> next) {
         this.item = item;
         this.previous = previous;
         this.next = next;
     }
 
+    /**
+     * Devuelve el elemento almacenado en este nodo.
+     *
+     * @return el elemento almacenado en este nodo.
+     */
     T getItem() {
         return item;
     }
 
+    /**
+     * Establece el elemento almacenado en este nodo.
+     *
+     * @param item el nuevo elemento a almacenar en este nodo.
+     */
     void setItem(T item) {
         this.item = item;
     }
 
+    /**
+     * Devuelve el nodo anterior a este nodo.
+     *
+     * @return el nodo anterior a este nodo.
+     */
     DequeNode<T> getPrevious() {
         return previous;
     }
 
+    /**
+     * Establece el nodo anterior a este nodo.
+     *
+     * @param previous el nuevo nodo anterior a este nodo.
+     */
     void setPrevious(DequeNode<T> previous) {
         this.previous = previous;
     }
 
+    /**
+     * Devuelve el nodo siguiente a este nodo.
+     *
+     * @return el nodo siguiente a este nodo.
+     */
     DequeNode<T> getNext() {
         return next;
     }
 
+    /**
+     * Establece el nodo siguiente a este nodo.
+     *
+     * @param next el nuevo nodo siguiente a este nodo.
+     */
     void setNext(DequeNode<T> next) {
         this.next = next;
     }
 
+    /**
+     * Devuelve verdadero si este nodo es el primer nodo de la lista.
+     *
+     * @return verdadero si este nodo es el primer nodo de la lista, falso de lo contrario.
+     */
     boolean isFirstNode() {
         return previous == null;
     }
 
+    /**
+     * Devuelve verdadero si este nodo es el último nodo de la lista.
+     *
+     * @return verdadero si este nodo es el último nodo de la lista, falso de lo contrario.
+     */
     boolean isLastNode() {
         return next == null;
     }
 
+    /**
+     * Devuelve verdadero si este nodo no es el primer ni el último nodo de la lista.
+     *
+     * @return verdadero si este nodo no es el primer ni el último nodo de la lista, falso de lo contrario.
+     */
     boolean isNotATerminalNode() {
         return !(isFirstNode() || isLastNode());
     }
--- src/main/java/colaDoble/DoublyLinkedListDeque.java
+++ src/main/java/colaDoble/DoublyLinkedListDeque.java
@@ -2,18 +2,31 @@ package colaDoble;
 
 import java.util.Comparator;
 
+/**
+
+ Implementación de una lista doblemente enlazada que permite agregar y eliminar elementos tanto al inicio como al final.
+
+ @param <T> el tipo de dato que se va a almacenar en la lista.
+ */
 public class DoublyLinkedListDeque<T> implements DoubleEndedQueue<T> {
 
     private DequeNode<T> first;
     private DequeNode<T> last;
     private int size;
 
+    /**
+     Crea una nueva lista doblemente enlazada vacía.
+     */
     public DoublyLinkedListDeque() {
         this.first = null;
         this.last = null;
         size = 0;
     }
 
+    /**
+     Agrega un elemento al inicio de la lista.
+     @param value el elemento a agregar.
+     */
     @Override
     public void prepend(T value) {
         DequeNode<T> Nodo = new DequeNode<>(value, null, first);
@@ -27,6 +40,10 @@ public class DoublyLinkedListDeque<T> implements DoubleEndedQueue<T> {
         size++;
     }
 
+    /**
+     Agrega un elemento al final de la lista.
+     @param value el elemento a agregar.
+     */
     @Override
     public void append(T value) {
         DequeNode<T> Nodo = new DequeNode<>(value, last, null);
@@ -40,11 +57,14 @@ public class DoublyLinkedListDeque<T> implements DoubleEndedQueue<T> {
         size++;
     }
 
+    /**
+     Elimina el primer elemento de la lista.
+     */
     @Override
     public void deleteFirst() {
         if (size > 1){
-           first = first.getNext();
-           first.setPrevious(null);
+            first = first.getNext();
+            first.setPrevious(null);
         } else if (size == 1){
             first = null;
             last = null;
@@ -54,6 +74,9 @@ public class DoublyLinkedListDeque<T> implements DoubleEndedQueue<T> {
         size--;
     }
 
+    /**
+     Elimina el último elemento de la lista.
+     */
     @Override
     public void deleteLast() {
         if (size > 1){
@@ -68,111 +91,154 @@ public class DoublyLinkedListDeque<T> implements DoubleEndedQueue<T> {
         size--;
     }
 
+    /**
+     Devuelve el primer elemento de la lista.
+     @return el primer elemento de la lista.
+     */
     @Override
     public T first() {
         return first.getItem();
     }
 
+    /**
+     Devuelve el nodo que representa al primer elemento de la lista.
+     @return el nodo que representa al primer elemento de la lista.
+     */
     public DequeNode<T> getFirst() {
         return first;
     }
 
+    /**
+     Devuelve el nodo que representa al último elemento de la lista.
+     @return el nodo que representa al último elemento de la lista.
+     */
     public DequeNode<T> getLast() {
         return last;
     }
 
+    /**
+     Devuelve el último elemento de la lista.
+     @return el último elemento de la lista.
+     */
     @Override
     public T last() {
         return last.getItem();
     }
 
+    /**
+     Devuelve el tamaño actual de la cola.
+     @return Tamaño actual de la cola.
+     */
     @Override
     public int size() {
         return size;
     }
 
+    /**
+     Devuelve el elemento de la cola que se encuentra en la posición indicada.
+     @param index Índice del elemento a devolver.
+     @return Elemento de la cola que se encuentra en la posición indicada.
+     @throws DoubleEndedQueueException Si el índice indicado es mayor que el tamaño de la lista.
+     */
     @Override
-    public T get(int index) {
+    public T get(int index) throws DoubleEndedQueueException {
         if(index > size){
             throw new DoubleEndedQueueException("El indice indicado es mayor que el tamaño de la lista");
         } else {
-            DequeNode aux = first;
+            DequeNode<T> aux = first;
             for(int i = 0; i < index; i++){
                 aux = aux.getNext();
             }
-            return (T) aux.getItem();
+            return aux.getItem();
         }
-
     }
 
+    /**
+     Comprueba si la cola contiene el elemento indicado.
+     @param value Elemento a buscar en la cola.
+     @return True si la cola contiene el elemento indicado, false en caso contrario.
+     @throws DoubleEndedQueueException Si el valor a buscar es null.
+     */
     @Override
-    public boolean contains(T value) {
+    public boolean contains(T value) throws DoubleEndedQueueException {
         if(value == null){
             throw new DoubleEndedQueueException("El valor a buscar no puede ser null");
         } else {
-            DequeNode aux = first;
+            DequeNode<T> aux = first;
             for(int i = 0; i < size; i++){
                 if(aux.getItem() == value){
                     return true;
                 }
+                aux = aux.getNext();
             }
             return false;
         }
     }
 
+    /**
+     Elimina el primer elemento de la cola que sea igual al elemento indicado.
+     @param value Elemento a eliminar de la cola.
+     @throws DoubleEndedQueueException Si el valor a buscar es null o si la cola está vacía.
+     */
     @Override
-    public void remove(T value) {
+    public void remove(T value) throws DoubleEndedQueueException {
         if(value == null){
             throw new DoubleEndedQueueException("El valor a buscar no puede ser null");
         } else {
             if(first.getItem() == value){
                 first = first.getNext();
-            }
-            if(last.getItem() == value){
+                first.setPrevious(null);
+            }else if(last.getItem() == value){
                 last = last.getPrevious();
-            }
-            DequeNode aux = first;
-            for(int i = 0; i < size; i++){
-                if(aux.getItem() == value){
-                    aux.getPrevious().setNext(aux.getNext());
-                    aux.getNext().setPrevious(aux.getPrevious());
+                last.setNext(null);
+            } else {
+                DequeNode<T> aux = first;
+                for(int i = 0; i < size; i++){
+                    if(aux.getItem() == value){
+                        aux.getPrevious().setNext(aux.getNext());
+                        aux.getNext().setPrevious(aux.getPrevious());
+                    }
+                    aux = aux.getNext();
                 }
             }
+            size--;
         }
     }
 
     /**
-     * Sorts the deque in non-descending order according to the order induced by the specified comparator.
-     *
-     * @param comparator the comparator to determine the order of the deque
+     Ordena la lista utilizando un comparador dado.
+     @param comparator el comparador utilizado para ordenar la lista.
+     @throws DoubleEndedQueueException si la lista tiene menos de dos elementos.
      */
-    @Override
-    public void sort(Comparator<? super T> comparator) {
-        if (size <= 1) {
-            return;
+    public void sort(Comparator<? super T> comparator) throws DoubleEndedQueueException {
+        if (size < 2) {
+            throw new DoubleEndedQueueException("No se puede ordenar una lista de 1 elemento");
         }
 
-        DequeNode<T> current = first.getNext();
-        while (current != null) {
-            DequeNode<T> insert = current;
-            T insertValue = insert.getItem();
-
-            DequeNode<T> node = current.getPrevious();
-            while (node != null && comparator.compare(insertValue, node.getItem()) < 0) {
-                node.getNext().setItem(node.getItem());
-                node = node.getPrevious();
+        boolean swapped;
+        do {
+            swapped = false;
+            DequeNode<T> current = first;
+            while (current.getNext() != null) {
+                DequeNode<T> next = current.getNext();
+                if (comparator.compare(current.getItem(), next.getItem()) > 0) {
+                    swapped = true;
+                    swapNodes(current, next);
+                } else {
+                    current = current.getNext();
+                }
             }
-
-            if (node == null) {
-                first.setItem(insertValue);
-            } else {
-                node.getNext().setItem(insertValue);
-            }
-            insert.setItem(null);
-            current = current.getNext();
-        }
+        } while (swapped);
     }
 
-
-
+    /**
+     Intercambia los elementos de dos nodos dados.
+     @param node1 el primer nodo.
+     @param node2 el segundo nodo.
+     */
+    private void swapNodes(DequeNode<T> node1, DequeNode<T> node2) {
+        T temp = node1.getItem();
+        node1.setItem(node2.getItem());
+        node2.setItem(temp);
+    }
 }
